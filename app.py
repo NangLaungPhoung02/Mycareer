11eeb69f8d11e3cc81a69c668b1428beb451c522
violexglajass <laungphoung.nang@my.jcu.edu.au>
2026-07-10 09:26:56 +0800

    instell flask and try simple run in broswer

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/applications")
+def applications():
+    return render_template("applications.html")
+
+
+@app.route("/add-application")
+def add_application():
+    return render_template("add_application.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
